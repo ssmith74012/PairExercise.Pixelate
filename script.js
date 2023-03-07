2c7ebc86611829753ba1731f822d4d768f4e5f64
NaseerHines <Hinesnaseer@gmail.com>
2023-03-06 19:25:16 -0600

    added fucntionality to create dynamic rows sort of

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your code here
+// creates table varible and uses html table
+const table = document.getElementById('table');
+
+// fucntion to make rows
+function makeRow() {
+    // makes a element table row
+    const tr = document.createElement('tr');
+    // loop thru and add cells to the row
+    for (let i = 0; i < 20; i++) {
+        const td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    // add row to table
+    table.appendChild(tr);
+}
+
+//invoke to add
+makeRow();
+makeRow();
